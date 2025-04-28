10c36f75eab7835400ae20548ef32906bb3bbc35
joshilopeze <joshilopeze>
2025-04-28 01:35:37 +0000

    creation of tables
    azure sql

---
+++ AzureSQLDatabase/tables.sql
@@ -0,0 +1,18 @@
+--Tables for the SQL DATABASE
+CREATE TABLE departments (
+    id INT PRIMARY KEY,
+    department VARCHAR(255) NOT NULL
+);
+CREATE TABLE jobs (
+    id INT PRIMARY KEY,
+    job VARCHAR(255) NOT NULL
+);
+CREATE TABLE hired_employees (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    datetime DATETIME NOT NULL,
+    department_id INT NOT NULL,
+    job_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id),
+    FOREIGN KEY (job_id) REFERENCES jobs(id)
+);
